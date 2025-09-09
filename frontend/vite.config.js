9f966bb6a448a9c1f3d1d3d0fb83f1a504148727
Deepak-Walunj <deepak.22211041@viit.ac.in>
2025-09-09 13:18:59 +0530

    initialised frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  assetsInclude: ['**/*.xlsx'],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1000,
+    },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@repository': path.resolve(__dirname, './src/repository'),
+    },
+  },
+})
