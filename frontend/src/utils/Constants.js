c48d44fc067e9490cc75a5f3139677bee11c71c5
Deepak-Walunj <deepak.22211041@viit.ac.in>
2025-09-11 22:41:38 +0530

    created user context

---
+++ frontend/src/utils/Constants.js
@@ -0,0 +1,9 @@
+export default {
+    LOCAL_STORAGE_ACCESS_TOKEN_KEY: 'access_token',
+    LOCAL_STORAGE_ACCESS_USER_INFO: 'user_info',
+    LOCAL_STORAGE_ACCESS_ENTITY_KEY: 'entity',
+    ENTITY: {
+        ADMIN: 'admin',
+        USER: 'user'
+    }
+}
